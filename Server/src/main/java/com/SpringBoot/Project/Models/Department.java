6ac60b11a53a90fbbde16dc1e67970916cd6901b
Henry <henrywinczner@gmail.com>
2025-01-28 13:08:48 -0500

    adding Entities

---
+++ Server/src/main/java/com/SpringBoot/Project/Models/Department.java
@@ -0,0 +1,51 @@
+package com.SpringBoot.Project.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long departmentId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = true)
+    private String description;
+
+    Department() {
+
+    }
+
+    public Department(Long departmentId, String name, String description){
+        this.departmentId = departmentId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Server/src/main/java/com/SpringBoot/Project/Models/Employee.java
@@ -0,0 +1,90 @@
+package com.SpringBoot.Project.Models;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employeeId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
+    @Column(nullable = false)
+    private String role;
+
+    Employee(){
+        //Default constructor for JPA
+    }
+
+    public Employee(long employeeId, String name, String email, Department department, String role){
+        this.employeeId = employeeId;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+        this.role = role;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return employeeId == employee.employeeId && Objects.equals(email, employee.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employeeId, email);
+    }
+
+}
