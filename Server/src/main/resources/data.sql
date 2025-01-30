2fbdc2c38fec63f7f0d658e9e6a598a5992756ba
Henry <henrywinczner@gmail.com>
2025-01-30 18:39:41 -0500

    trying to get server to see sql data

---
+++ Server/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (department_id, name, description) 
+  VALUES (1, 'IT', 'Information Technology');
+INSERT INTO department (department_id, name, description) 
+  VALUES (2, 'HR', 'Human Resources');
+
+INSERT INTO employee (employee_id, name, email, role, department_id)
+  VALUES (1, 'Alice Johnson', 'alice@example.com', 'Manager', 1);
