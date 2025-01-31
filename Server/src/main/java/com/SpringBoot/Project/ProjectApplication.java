a2ba1a7b3b4541c01613050ee319c1cf090f3d87
Henry <henrywinczner@gmail.com>
2025-01-31 15:10:01 -0500

    department http requests are done, post for employee is working as well

--- Server/src/main/java/com/SpringBoot/Project/ProjectApplication.java
+++ Server/src/main/java/com/SpringBoot/Project/ProjectApplication.java
@@ -1,13 +1,7 @@
 package com.SpringBoot.Project;
 
-import com.SpringBoot.Project.Models.Department;
-import com.SpringBoot.Project.Models.Employee;
-import com.SpringBoot.Project.Repositories.DepartmentInterface;
-import com.SpringBoot.Project.Repositories.EmployeeInterface;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectApplication {
